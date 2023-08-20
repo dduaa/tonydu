ebf0e44146337112f4108bb15c85c22363f5ad6a
DONGHONG DU <east.ddh@gmail.com>
2023-08-20 22:51:33 +0100

    add firebase

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  webpack: (config) => {
-    config.resolve.extensionAlias = {
-   ".js": [".ts", ".tsx", ".js", ".jsx"],
-   ".mjs": [".mts", ".mjs"],
-   ".cjs": [".cts", ".cjs"],
- };
- return config;
-},
 }
 
 const withMDX = nextMDX({
